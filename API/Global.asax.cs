11cf62d6f6333baa393dc0ce450bb709e0c71fe7
Andres <dandresleon64@gmail.com>
2020-05-15 11:07:28 -0500

    subiendo arquitectura inicial

---
+++ API/Global.asax.cs
@@ -0,0 +1,26 @@
+namespace API
+{
+    using System;
+    using System.Web;
+    using System.Web.Http;  
+
+    public class WebApiApplication : System.Web.HttpApplication
+    {
+        protected void Application_Start()
+        {
+            GlobalConfiguration.Configure(WebApiConfig.Register);
+        }
+
+        protected void Application_BeginRequest(object sender, EventArgs e)
+        {
+            
+            HttpContext.Current.Response.AddHeader("Access-Control-Allow-Origin", "*");
+            HttpContext.Current.Response.AddHeader("Access-Control-Allow-Headers", "Content-Type, Accept");
+            if (HttpContext.Current.Request.HttpMethod == "OPTIONS")
+            {
+                HttpContext.Current.Response.AddHeader("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE");
+                HttpContext.Current.Response.End();
+            }
+        }
+    }
+}
---
+++ Base.IC/Class1.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Base.IC
+{
+    public class Class1
+    {
+    }
+}
